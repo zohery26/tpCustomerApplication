af2e40c8f255568fb5ffc6b7d20ac1a3b81bd262
Razafimanantsoa Zo Hery Ny Aina Dominique <zoherynyaina@gmail.com>
2022-03-24 16:33:47 +0300

    Ajout EJB CustomerManager

---
+++ src/main/java/mg/zo/tpcustomerapplication/session/CustomerManager.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mg.zo.tpcustomerapplication.session;
+
+
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import mg.zo.tpcustomerapplication.entities.Customer;
+
+/**
+ *
+ * @author macbookair
+ */
+@Stateless
+public class CustomerManager {
+    @PersistenceContext(unitName = "customerPU")
+    private EntityManager em;
+
+    public void persist(Object customer) {
+      em.persist(customer);
+    }
+
+    /**
+     *
+     * @return
+     */
+    public List<Customer> getAllCustomers() {
+      Query query = em.createNamedQuery("Customer.findAll");
+      return query.getResultList();
+    }
+
+    public Customer update(Customer customer) {
+      return em.merge(customer);
+    }
+
+    public void persist(Customer customer) {
+       em.persist(customer);
+    }
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
